c489db7ea3e0aeb234d752afd06b769de26c8129
Hilmi <hilmi.m.arifin@gmail.com>
2023-01-23 20:02:52 +0700

    initial set up project

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import cors from 'cors'
+import dotenv from "dotenv";
+import sequelizeConnection from "./config/dbConnect";
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    return res.status(200).send({
+        response: "Express TypeScript"
+    });
+});
+
+
+(async () => {
+    try {
+        await sequelizeConnection.authenticate()
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})()
+
+
+app.listen(process.env.APP_PORT, () => {
+    console.log((`${process.env.APP_NAME} running on port ${process.env.APP_PORT}`))
+});
