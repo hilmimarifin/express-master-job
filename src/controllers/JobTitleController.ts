3a88b53bf81bdd801c5445a95493878b66ec3e76
Hilmi <hilmi.m.arifin@gmail.com>
2023-01-25 02:57:49 +0700

    add update and delete API for job title

--- src/controllers/JobTitleController.ts
+++ src/controllers/JobTitleController.ts
@@ -56,4 +56,83 @@ const GetList = async (req: Request, res: Response): Promise<Response> => {
         });
     }
 };
-export default {Create, GetList}
+
+const Update = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { code, name, id } = req.body;
+
+        const title = await JobTitle.findByPk(id);
+
+        if (!title) {
+            return res.status(404).send({
+                status: 404,
+                message: "Data Not Found",
+                data: null
+            });
+        }
+
+        title.name = name;
+        title.code = code;
+
+        await title.save();
+
+        return res.status(200).send({
+            status: 200,
+            message: "OK",
+            data: title
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error
+            });
+        }
+
+        return res.status(500).send({
+            status: 500,
+            message: "Internal server error",
+            errors: error
+        });
+    }
+};
+
+const Delete = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.body;
+
+        const role = await JobTitle.findByPk(id);
+
+        if (!role) {
+            return res.status(404).send({
+                status: 404,
+                message: "Data Not Found",
+                data: null
+            });
+        }
+
+        await role.destroy();
+
+        return res.status(200).send({
+            status: 200,
+            message: "Deleted",
+            data: null
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error
+            });
+        }
+
+        return res.status(500).send({
+            status: 500,
+            message: "Internal server error",
+            errors: error
+        });
+    }
+}
+export default {Create, GetList, Update, Delete}
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -7,5 +7,9 @@ const router = express.Router();
 //job title routes
 router.post("/job-title", JobTitleController.Create);
 router.get("/job-title", JobTitleController.GetList);
+router.patch("/job-title/update", JobTitleController.Update);
+router.delete("/job-title/delete", JobTitleController.Delete);
+
+
 
 export default router
